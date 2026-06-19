d7e7674886e5b9eb1b1fa62b84a4e7908727bb44
umaremrunal0-spec <umaremrunal0@gmail.com>
2026-06-19 19:54:53 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,34 @@
+INSERT INTO users VALUES
+(1,'Rahul',22,'India','Premium'),
+(2,'Priya',20,'India','Basic'),
+(3,'John',25,'USA','Premium'),
+(4,'Sara',24,'UK','Standard'),
+(5,'Aman',21,'India','Premium');
+
+INSERT INTO movies VALUES
+(101,'Money Heist','Crime',2017,60),
+(102,'Stranger Things','Sci-Fi',2016,50),
+(103,'Dark','Mystery',2017,55),
+(104,'Wednesday','Fantasy',2022,45),
+(105,'Squid Game','Thriller',2021,55);
+
+INSERT INTO watch_history VALUES
+(1,1,101,'2026-01-01',60),
+(2,2,102,'2026-01-02',50),
+(3,3,103,'2026-01-03',55),
+(4,1,104,'2026-01-04',45),
+(5,5,105,'2026-01-05',55);
+
+INSERT INTO ratings VALUES
+(1,1,101,4.5),
+(2,2,102,5.0),
+(3,3,103,4.8),
+(4,1,104,4.2),
+(5,5,105,4.9);
+
+INSERT INTO subscriptions VALUES
+(1,1,499,'2025-01-01'),
+(2,2,199,'2025-02-01'),
+(3,3,499,'2025-03-01'),
+(4,4,299,'2025-04-01'),
+(5,5,499,'2025-05-01');
